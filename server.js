48e4e3b9383c71ac7b792e94698f5a6d89e408c5
Andres F. Bonilla Q. <mailofbonilla@gmail.com>
2024-11-26 12:17:04 -0500

    :rocket: deploy

---
+++ server.js
@@ -0,0 +1,38 @@
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+import next from 'next';
+
+const dev = 'production';
+const hostname = 'localhost';
+const port = 3000;
+
+// When using middleware `hostname` and `port` must be provided below.
+const app = next({ dev, hostname, port });
+
+const handler = app.getRequestHandler();
+
+app.prepare().then(() => {
+   const httpServer = createServer(handler);
+
+   const io = new Server(httpServer);
+
+   io.on('connection', (socket) => {
+      console.log('socket-id:', socket.id);
+
+      socket.on('message', (body) => {
+         socket.broadcast.emit('message', {
+            body,
+            from: socket.id.slice(8)
+         });
+      });
+   });
+
+   httpServer
+      .once('error', (err) => {
+         console.error(err);
+         process.exit(1);
+      })
+      .listen(port, () => {
+         console.log(`> Ready on http://${hostname}:${port}`);
+      });
+});
